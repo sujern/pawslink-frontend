331a34d4033dd0b634db9c9bd6da401844957c9b
sujern <sujern7856@gmail.com>
2025-03-18 16:47:17 +0700

    signupdemo2

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -77,7 +77,7 @@ const Login = () => {
             onClick={() => navigate("/register")}
             className="text-yellow-500 font-semibold  hover:underline ml-1"
           >
-            Sign upppppppp!!
+            Sign uppp!!
           </button>
         </p>
       </div>
